a228fe9ca7ac88e365eb50678f425193ece3d597
hituhin <hituhin09@gmail.com>
2022-06-26 00:04:14 +0600

    first page connected with component

---
+++ components/material-ui-design/MaterialUi.js
@@ -0,0 +1,12 @@
+//import react from 'next/react'
+
+import React from "react";
+const MaterialUi = () => {
+
+    return (
+        <div> I am here</div>
+    )
+
+
+}
+export default MaterialUi;
---
+++ pages/material-ui-desigin/index.js
@@ -0,0 +1,11 @@
+//import react from "next/react";
+import React from "react";
+import MatarialUiDesign from "../../components/material-ui-design/MaterialUi"
+
+const index = () => {
+
+    return (
+        <MatarialUiDesign />
+    )
+}
+export default index;
